af27f9f5c2e3ee0a37c9f9afacf4808c5d7017af
amercado <arnold.mercado@hotmail.com>
2024-03-30 18:29:05 +0300

    Initial dashboard with user management

---
+++ constants/index.tsx
@@ -0,0 +1,27 @@
+export const sideBarLinks = [
+   {
+      label: "Home",
+      route: "/",
+      imgUrl: "/icons/Home.svg",
+   },
+   {
+      label: "Upcoming Meeting",
+      route: "/upcoming",
+      imgUrl: "/icons/upcoming.svg",
+   },
+   {
+      label: "Previous Meeting",
+      route: "/previous",
+      imgUrl: "/icons/previous.svg",
+   },
+   {
+      label: "Recordings",
+      route: "/recordings",
+      imgUrl: "/icons/Video.svg",
+   },
+   {
+      label: "Personal Room",
+      route: "/personal-room",
+      imgUrl: "/icons/add-personal.svg",
+   },
+];
